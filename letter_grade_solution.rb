1b448c33084e4f458b659ad33199633fd2c1e883
Chris Bowen <chrisbowen@comcast.net>
2022-10-05 17:12:20 -0500

    answered questions and pseudocode

--- letter_grade_solution.rb
+++ letter_grade_solution.rb
@@ -3,10 +3,11 @@
 #  Answer the research questions below
   # 1. What happens when you make a method with no parameters then pass in an argument?
 
-    # 
+    # Unexpected number of arguments error(expect 0 got 1)
 
   # 2. What happens when you have a method that defines three parameters and you give it only 2 arguments when it is called?
 
+    # Unexpected number of arguments error(expected 3 got 2)
     # 
 
   # 3. What are the errors you get?
@@ -23,13 +24,13 @@
 
 # Commit 2 - Pseudocode
 
-# input: 
-# output: 
+# input: an array of integers
+# output: a letter grade based on the mean of the grades
 
 # Steps
-# 1 - 
-# 2 -
-# 3 - 
+# 1 - Take in a series of numbers
+# 2 - Find the average of those numbers
+# 3 - Give a letter grade based on that average
 
 # Commit 3 - Initial Solution
 
@@ -42,5 +43,6 @@ end
 
 
 # Commit 4 - Write Runner Code / Tests
+
 
 
