0d3ff89e699f7be6fbcfcc6b7339c049ba892826
Chris Bowen <chrisbowen@comcast.net>
2022-10-05 18:03:36 -0500

    bonus solution of reading and parsing a csv file

--- letter_grade_solution.rb
+++ letter_grade_solution.rb
@@ -34,36 +34,73 @@
 
 # Commit 3 - Initial Solution
 
-def calculate_letter_grade(*grades)
-  length = grades.length
-  total_grade = 0
-  grades.each do |grade|
-    total_grade += grade
-  end
-  avg = total_grade / length
+require 'csv'
 
-  if avg > 89
-    if avg > 97 then return 'A+' elsif avg < 93 then return 'A-' else return 'A' end
-  elsif avg > 79
-    if avg > 87 then return 'B+' elsif avg < 83 then return 'B-' else return 'B' end
-  elsif avg > 69
-    if avg > 77 then return 'C+' elsif avg < 73 then return 'C-' else return 'C' end
-  elsif avg > 59
-    if avg > 67 then return 'D+' elsif avg < 63 then return 'D-' else return 'D' end
-  else  
-    return 'F :('
+students = CSV.read("resources/grades.csv")
+
+students.each do |student|
+  name = student[0]
+  grades = student.length - 1
+  total_grades = 0
+  index = 1
+
+  grades.times do
+    total_grades += student[index].to_i
   end
+
+  avg = total_grades / grades
+  letter_grade = ''
+
+   if avg > 89
+      if avg > 97 then letter_grade = 'A+' elsif avg < 93 then letter_grade = 'A-' else letter_grade = 'A' end
+    elsif avg > 79
+      if avg > 87 then letter_grade = 'B+' elsif avg < 83 then letter_grade = 'B-' else letter_grade = 'B' end
+    elsif avg > 69
+      if avg > 77 then letter_grade = 'C+' elsif avg < 73 then letter_grade = 'C-' else letter_grade = 'C' end
+    elsif avg > 59
+      if avg > 67 then letter_grade = 'D+' elsif avg < 63 then letter_grade = 'D-' else letter_grade = 'D' end
+    else  
+      letter_grade = 'F :('
+    end
+  
+  puts "#{name} got an avgerage of #{avg} and recieved a #{letter_grade}"
 end
 
+# CSV.foreach("resources/grades.csv") do |row|
+#     puts row 
+# end 
 
-# Commit 4 - Write Runner Code / Tests
 
-p calculate_letter_grade(65, 70, 75)
-p calculate_letter_grade(95, 98, 76, 94)
-p calculate_letter_grade(100)
-p calculate_letter_grade(85, 80, 90, 89, 87, 86, 82)
-p calculate_letter_grade(45, 75, 67, 85, 90)
-p calculate_letter_grade(45, 60, 67, 25, 0)
+# def calculate_letter_grade(*grades)
+#   length = grades.length
+#   total_grade = 0
+#   grades.each do |grade|
+#     total_grade += grade
+#   end
+#   avg = total_grade / length
+
+#   if avg > 89
+#     if avg > 97 then return 'A+' elsif avg < 93 then return 'A-' else return 'A' end
+#   elsif avg > 79
+#     if avg > 87 then return 'B+' elsif avg < 83 then return 'B-' else return 'B' end
+#   elsif avg > 69
+#     if avg > 77 then return 'C+' elsif avg < 73 then return 'C-' else return 'C' end
+#   elsif avg > 59
+#     if avg > 67 then return 'D+' elsif avg < 63 then return 'D-' else return 'D' end
+#   else  
+#     return 'F :('
+#   end
+# end
+
+
+# # Commit 4 - Write Runner Code / Tests
+
+# p calculate_letter_grade(65, 70, 75)
+# p calculate_letter_grade(95, 98, 76, 94)
+# p calculate_letter_grade(100)
+# p calculate_letter_grade(85, 80, 90, 89, 87, 86, 82)
+# p calculate_letter_grade(45, 75, 67, 85, 90)
+# p calculate_letter_grade(45, 60, 67, 25, 0)
 
 
 
